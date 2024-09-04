5962d5a4a9e521bed2522b7df9cac82896122090
JoshGersh <josh.gershuny@snhu.edu>
2024-09-04 15:29:31 -0400

    Added the collect method

--- entrypoint.py
+++ entrypoint.py
@@ -37,4 +37,30 @@ def record_engine_temperature():
 # practically identical to the above
 @app.route('/collect', methods=['POST'])
 def collect_engine_temperature():
-    return {"success": True}, 200
+    # Connect to the Redis database
+    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+
+    # Retrieve a list of engine temperature values from Redis
+    engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
+    logger.info(f"recived engine temperature successful from redis: {engine_temperature_values}")
+
+    # Convert strings to floats (or int if you're sure the values are whole numbers)
+    engine_temperature_sum = 0
+    for temp in engine_temperature_values:
+        engine_temperature_sum += float(temp)
+
+    # Calculate the average of the engine temperature values
+    average_engine_temperature = engine_temperature_sum / len(engine_temperature_values)
+
+    logger.info(f"calculated average engine temperature: {average_engine_temperature}")
+    logger.info(f"returning current engine temperature: {engine_temperature_values[0]}")
+
+    # Prepare the result dictionary to be returned as a JSON response
+    result = {
+        "current_engine_temperature": engine_temperature_values[0],
+        "average_engine_temperature": average_engine_temperature
+    }
+    logger.info(f"collect request successful")
+
+    # Return the result with a 200 OK status code
+    return result, 200
